2ff82d268b59b141c597283ef69aaba44b84ec41
Govind V R <govindvr02@gmail.com>
2023-07-06 22:40:42 +0530

    feat: form for new bill

--- app/screens/NewBill.js
+++ app/screens/NewBill.js
@@ -1,11 +1,41 @@
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet,TextInput } from 'react-native';
 import colors from '../config/colors';
+import { useState,useEffect } from 'react';
+import {getLastInvoiceNumber} from '../config/supabaseClient';
 
 function NewBill() {
+    const [invoiceNumber, setInvoiceNumber] = useState("");
+    useEffect(() => {
+        fetchInvoiceNumber();
+      }, []);
+
+      async function fetchInvoiceNumber() {
+        const number = await getLastInvoiceNumber();
+        setInvoiceNumber(number || 'N/A');
+      }
+
+      console.log(typeof(invoiceNumber));
     return (
+    
         <SafeAreaView style={styles.container}>
-        <View >
-            <Text>New Bill</Text>
+        <View style={styles.twoItemBox}>
+            <View style={styles.inputBox}>
+                <Text style={styles.label}>{"Invoice Number"}</Text>
+                <TextInput
+                    style={styles.input}
+                    value={invoiceNumber}
+                    keyboardType="numeric"
+                    defaultValue = {invoiceNumber}                
+                />
+            </View>
+            <View style={styles.inputBox}>
+                <Text style={styles.label}>{"Invoice Number"}</Text>
+                <TextInput
+                    style={styles.input}
+                    value={2}
+                 
+                />
+            </View>
         </View>
         </SafeAreaView>
     );
@@ -15,5 +45,24 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.primary,
     },
+    twoItemBox: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    inputBox: {
+        marginBottom: 20,
+    },label: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 5,
+      },
+      input: {
+        height: 40,
+        width: "80%",
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+      },
 });
 export default NewBill;
