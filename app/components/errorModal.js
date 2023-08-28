d390ab2343a15d1d0edfe13c9345a6112744b692
Govind V R <govindvr02@gmail.com>
2023-08-29 00:19:12 +0530

    feat: Error Model Component Added

---
+++ app/components/errorModal.js
@@ -0,0 +1,53 @@
+import colors from '../config/colors';
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from 'react-native';
+
+
+function ErrorModal({ visible, message, onClose }) {
+  return (
+    <Modal visible={visible} animationType="fade" transparent>
+        <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+            <Text style={styles.modalText}>{message}</Text>
+            <TouchableOpacity style={styles.modalButton} onPress={onClose}>
+            <Text style={styles.modalButtonText}>OK</Text>
+            </TouchableOpacity>
+        </View>
+        </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+    error: {
+        color: 'red',
+        marginBottom: 8,
+      },
+      modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      },
+      modalContent: {
+        backgroundColor: 'white',
+        padding: 16,
+        borderRadius: 8,
+        alignItems: 'center',
+      },
+      modalText: {
+        fontSize: 16,
+        marginBottom: 16,
+      },
+      modalButton: {
+        backgroundColor: colors.secondary,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderRadius: 4,
+      },
+      modalButtonText: {
+        color: 'white',
+        fontSize: 16,
+      },
+})
+
+export default ErrorModal
