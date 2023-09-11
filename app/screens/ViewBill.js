0cc30c50d1045a2f5c6ce3f4dcde7f7acd7f2fe8
Govind V R <govindvr02@gmail.com>
2023-09-11 22:15:03 +0530

    Revert "feat: Print Bill Feature Added"
    This reverts commit 96444c997ff4d90c29cb0749533c53799ba23fcf.

--- app/screens/ViewBill.js
+++ app/screens/ViewBill.js
@@ -2,10 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Linking,ScrollView } from 'react-native';
 import { getBill } from '../config/supabaseClient';
 import OpenLinkButton from '../components/printButton';
-import { API_URL } from '@env';
-
-
-const url=API_URL;
 
 function ViewBill({ route }) {
 
@@ -33,35 +29,6 @@ function ViewBill({ route }) {
     );
   }
 
-  const handlePrintBill = async () => {
-    delete bill.billitems;
-    req_body = {
-      "bill": bill,
-      "products": products
-    }
-    const uri = "https://gst-bill-backend.onrender.com/" + "api/sheets/setdata";
-    try {
-      const response = await fetch(uri, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(req_body),
-      });
-
-
-      if(response.status != 201)
-      {
-        console.log(response.status);
-        console.log(response.statusText);
-        console.log(response);
-        return;
-      }
-    } catch (error) {
-      console.error('Error sending request:', error);
-    }
-  }
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.heading}>Bill Details</Text>
@@ -162,10 +129,7 @@ function ViewBill({ route }) {
             
         </View>
       ))}
-      <OpenLinkButton 
-        url="https://docs.google.com/spreadsheets/d/1czE1-2jvlOsDIBBcXHaN4zF3-WeWkHbwRumcuDNcFYc/export?format=pdf&portrait=true&size=A4" 
-        handleFunction={handlePrintBill}
-      />
+      <OpenLinkButton url="https://docs.google.com/spreadsheets/d/1czE1-2jvlOsDIBBcXHaN4zF3-WeWkHbwRumcuDNcFYc/export?format=pdf&portrait=true&size=A4" />
 
     </ScrollView>
   );
